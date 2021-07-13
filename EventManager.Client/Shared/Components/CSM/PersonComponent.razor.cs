9f28a694a561c12d71090a6e8aa1bc63b4c9a26c
Karcag Tamas <karcagtamas@outlook.com>
2021-07-13 15:20:03 +0200

    #84 Person status list modify

--- EventManager.Client/Shared/Components/CSM/PersonComponent.razor.cs
+++ EventManager.Client/Shared/Components/CSM/PersonComponent.razor.cs
@@ -1,4 +1,5 @@
 ﻿using ManagerAPI.Shared.Enums;
+using ManagerAPI.Shared.Helpers;
 using ManagerAPI.Shared.Models.CSM;
 using Microsoft.AspNetCore.Components;
 using System;
@@ -44,6 +45,14 @@ namespace EventManager.Client.Shared.Components.CSM
 
         private bool IsEdit { get; set; } = false;
         private bool IsOpened { get; set; } = false;
+        
+        private List<PersonStatus> _statusList = new();
+
+        /// <inheritdoc />
+        protected override void OnParametersSet()
+        {
+            this.SetAvailableStatuses();
+        }
 
         private void ChangeIsAvailableStatus(PersonTableModel model, bool value)
         {
@@ -71,7 +80,7 @@ namespace EventManager.Client.Shared.Components.CSM
         {
             if (value)
             {
-                if (!person.IgnoredWorks.Any(x => x == work.Id))
+                if (person.IgnoredWorks.All(x => x != work.Id))
                 {
                     person.IgnoredWorks.Add(work.Id);
                 }
@@ -84,6 +93,54 @@ namespace EventManager.Client.Shared.Components.CSM
                 }
             }
             this.StateChanged.InvokeAsync();
+        }
+
+        private void SetAvailableStatuses()
+        {
+            var list = new List<PersonStatus>();
+            bool? lastStatus = null;
+            
+            Person.Tables.ForEach(table =>
+            {
+                bool val = table.IsAvailable;
+
+                if (lastStatus is null)
+                {
+                    list.Add(new PersonStatus {Date = table.Date, Length = 1, Value = GetStatus(val)});
+                    lastStatus = val;
+                } 
+                else if (lastStatus == val)
+                {
+                    list[^1].Length++;
+                }
+                else
+                {
+                    list.Add(new PersonStatus {Date = table.Date, Length = 1, Value = GetStatus(val)});
+                    lastStatus = val;
+                }
+            });
+
+            this._statusList = list;
+        }
+
+        private string GetStatus(bool status)
+        {
+            return status ? "Available" : "Not Available";
+        }
+    }
+
+    class PersonStatus 
+    {
+        public DateTime Date { get; set; }
+        public string Value { get; set; }
+        public int Length { get; set; }
+
+        public string Interval
+        {
+            get
+            {
+                return WriteHelper.HourInterval(Date, Length);
+            }
         }
     }
 }
